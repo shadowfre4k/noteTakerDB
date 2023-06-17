166ef83bfa01d9f8ac475b3d921141c9351f8cf7
Vincent Jacobs <86565286+shadowfre4k@users.noreply.github.com>
2023-06-16 18:16:29 -0700

    server and route changes

--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,9 @@
 const express = require("express");
 
-// Import our modular routers for /tips and /feedback
-
 const notesRouter = require("./notes");
-// TODO: import your diagnostics route
 
 const app = express();
 
 app.use("/notes", notesRouter);
-// TODO: Initialize diagnostics route
 
 module.exports = app;
